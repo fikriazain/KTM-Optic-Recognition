e056cc161740d9df83ee12a7e9ba71ffa0791ae7
Fikri Aufaa Zain <fikri.azain@gmail.com>
2022-07-25 16:16:15 +0700

    add prodi Ilmu hukum, teknik mesin, Fisika

--- prodi_and_angkatan.py
+++ prodi_and_angkatan.py
@@ -1,7 +1,7 @@
 angkatan = {
     #Fill it with your faculty batch
     "S1":{
-        "Fasilkom": {
+        "FASILKOM": {
                 "180" : "Quanta",
                 "190" : "Maung",
                 "200" : "Chronos",
@@ -12,12 +12,35 @@ angkatan = {
 
 prodi = {
     "00.12.01":{
-        "01": ['Reguler', 'S1 Ilmu Komputer', 'Fasilkom', 'S1'],
-        "02": ['Kls Internasional', 'S1 Ilmu Komputer', 'Fasilkom', 'S1'],
-        "06": ['Reguler', 'S1 Sistem Informasi', 'Fasilkom', 'S1'],
-        "07": ['Ekstensi', 'S1 Sistem Informasi', 'Fasilkom', 'S1'],
-        "08": ['Paralel', 'S1 Sistem Informasi', 'Fasilkom', 'S1'],
-        "09": ['Paralel', 'S1 Ilmu Komputer', 'Fasilkom', 'S1'],
+        #FASILKOM
+        "01": ['Reguler', 'S1 Ilmu Komputer', 'FASILKOM', 'S1'],
+        "02": ['Kls Internasional', 'S1 Ilmu Komputer', 'FASILKOM', 'S1'],
+        "06": ['Reguler', 'S1 Sistem Informasi', 'FASILKOM', 'S1'],
+        "07": ['Ekstensi', 'S1 Sistem Informasi', 'FASILKOM', 'S1'],
+        "08": ['Paralel', 'S1 Sistem Informasi', 'FASILKOM', 'S1'],
+        "09": ['Paralel', 'S1 Ilmu Komputer', 'FASILKOM', 'S1'],
+    },
+    "00.05.01":{
+        #FH Ilmu Hukum
+        "01": ['Reguler', 'S1 Ilmu Hukum', 'FH', 'S1'],
+        "02": ['Ekstensi', 'S1 Ilmu Hukum', 'FH', 'S1'],
+        "06": ['Paralel', 'S1 Ilmu Hukum', 'FH', 'S1'],
+        "07": ['Kls Internasional', 'S1 Ilmu Hukum', 'FH', 'S1'],
+    },
+    "02.04.01":{
+        #FT Teknik Mesin
+        "01": ['Reguler', 'S1 Teknik Mesin', 'FT', 'S1'],
+        "03": ['Ekstensi', 'S1 Teknik Mesin', 'FT', 'S1'],
+        "04": ['Kls Internasional', 'S1 Teknik Mesin', 'FT', 'S1'],
+        "07": ['Paralel', 'S1 Teknik Mesin', 'FT', 'S1'],
+    }, 
+    "02.03.01":{
+        #FMIPA Fisika/Geofisika
+        "02": ['Reguler', 'S1 Fisika', 'FMIPA', 'S1'],
+        "03": ['Ekstensi', 'S1 Fisika', 'FMIPA', 'S1'],
+        "05": ['Paralel', 'S1 Fisika', 'FMIPA', 'S1'],
+        "06": ['Reguler', 'S1 Geofisika', 'FMIPA', 'S1'],
+        "09": ['Paralel', 'S1 Geofisika', 'FMIPA', 'S1'],
     }
 }
 
