eadf452a8b3cc45d4f7ea404befc4185a0f9d464
Fikri Aufaa Zain <fikri.azain@gmail.com>
2022-07-28 15:23:08 +0700

    make detect py

--- detect.py
+++ detect.py
@@ -1,15 +1,15 @@
+from msilib.schema import File
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import easyocr
 from utils_me import rectangle, resize, putText, extract_npm, extract_prodi
 
-def detect(image):
+def detect_show(image):
     scale = round(500 / image.shape[0], 1)
     image = resize(image, scale)
     reader = easyocr.Reader(['id', 'en'])
     result = reader.readtext(image)
-    result
 
     img = rectangle(image, result)
 
@@ -17,8 +17,17 @@ def detect(image):
     npm = extract_npm(result, detail[2], detail[3])
     img = putText(image, detail, npm)
 
+    try:
+        os.mkdir("ImageOut")    
+    except FileExistsError:
+        pass
+
     cv2.imshow("Your ID",image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+def detect_multiple(img):
+    pass
 
 
